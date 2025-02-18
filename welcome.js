82f8567a3d22ec555bb6287a6caf93cf9642ae04
dpuentex <dpuente1097@gmail.com>
2025-02-17 19:01:21 -0800

    cookie-message to welcome page using JS

---
+++ welcome.js
@@ -0,0 +1,11 @@
+const welcomePage_header = document.querySelector(".welcomePage_header");
+const cookie_message = document.createElement("div");
+cookie_message.classList.add("cookie-message");
+cookie_message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it</button>`;
+welcomePage_header.append(cookie_message);
+
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    cookie_message.remove();
+  });
